32c5f8aa7e860e7a8ec555f5b547cf6edebd6ecd
Acejoy <srinjoym98@gmail.com>
2021-06-09 12:34:48 +0530

    modified 79

--- 79. Word Search/my_solution.cpp
+++ 79. Word Search/my_solution.cpp
@@ -7,50 +7,54 @@ map<string, pair<int, int>> neighbors = {{"left", make_pair(0, -1)},
                                          {"bottom", make_pair(+1, 0)}};
 
 bool existHelper(vector<vector<char>>& board, string word,
-                 pair<int, int> curPos, set<pair<int, int>>& visited,
-                 int wordIdx) {
+                 pair<int, int> curPos, int wordIdx,
+                 set<pair<int, int>> visited) {
     if (wordIdx == word.size()) {
         return true;
     } else {
-        cout << "current Pos" << curPos.first << ' ' << curPos.second << '\n';
-        // visited.insert(curPos);
-
-        // neighbors
-
         int totalRows = board.size();
         int totalCols = board[0].size();
         pair<int, int> newPos;
+
+        cout << "current Pos" << curPos.first << ' ' << curPos.second << '\n';
+
+        if (curPos.first < 0 || curPos.first >= totalRows ||
+            curPos.second < 0 || curPos.second >= totalCols) {
+            return false;
+        }
+
+        if (board[curPos.first][curPos.second] != word[wordIdx]) {
+            cout << "Returning\n";
+            return false;
+        }
+
+        visited.insert(curPos);
+
         for (auto e : neighbors) {
-            
-            newPos = make_pair(curPos.first+e.second.first, curPos.second+e.second.second);
+            newPos = make_pair(curPos.first + e.second.first,
+                               curPos.second + e.second.second);
 
-            if(newPos.first<0 || newPos.first>=totalRows || newPos.second<0 || newPos.second>=totalCols){
+            if (visited.find(newPos) != visited.end()) {
                 continue;
             }
 
-            if(visited.find(newPos)!=visited.end()){
-                continue;
+            cout << "Going " << e.first << ".....\n";
+            bool res = existHelper(board, word, newPos, wordIdx + 1, visited);
+            if (res) {
+                return true;
             }
-
-            // if(board[newPos.first][newPos.second]==word[wordIdx]){
-                cout<<"Going "<<e.first<<".....\n";
-                bool res = existHelper(board, word, newPos, visited, wordIdx + 1);
-                if(res){
-                    return true;
-                }
-            //}
         }
         return false;
     }
 }
 
 bool exist(vector<vector<char>>& board, string word) {
+    set<pair<int, int>> s;
     for (int row = 0; row < board.size(); row++) {
         for (int col = 0; col < board[0].size(); col++) {
             if (board[row][col] == word[0]) {
                 cout << "============\n";
-                set<pair<int, int>> s;
-                bool res = existHelper(board, word, make_pair(row, col), s, 1);
+                bool res = existHelper(board, word, make_pair(row, col), 0, s);
                 if (res) {
                     return true;
                 }
@@ -62,10 +66,13 @@ bool exist(vector<vector<char>>& board, string word) {
 }
 
 int main() {
-    vector<vector<char>> board = {
-        {'A', 'B', 'C', 'E'}, {'S', 'F', 'E', 'S'}, {'A', 'D', 'E', 'E'}};
-    string word = "ABCESEEEFS";
+    vector<vector<char>> board1 = {
+        {'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
 
-    cout << exist(board, word);
+    vector<vector<char>> board2 = {{'a'}};    
+    string word1 = "ABCB";
+    string word2 = "a";
+
+    cout << exist(board2, word2);
     return 0;
 }
